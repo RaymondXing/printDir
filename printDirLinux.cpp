a41b45a7a4062fea9a9946fd3484f23215569bfd
xing rui <30753546+RaymondXing@users.noreply.github.com>
2021-10-31 14:35:13 +0800

    Add files via upload

---
+++ printDirLinux.cpp
@@ -0,0 +1,47 @@
+//示例程序：输出当前目录下的文件
+//编译(compile)：g++ printDirLinux.cpp -o printDirLinux -O3 -std=c++11
+//运行(execute)：./printDirLinux
+#include <iostream>
+#include <vector>
+#include <string>
+#include <dirent.h>
+
+//输入文件夹路径dir_name，输出文件夹列表dir与文件列表file
+int find_dir_file(std::string dir_name, std::vector<std::string>& dir, std::vector<std::string>& file)
+{
+	DIR *dirp;
+	struct dirent *dp;
+	dirp = opendir(dir_name.c_str());
+	if(dirp == NULL)
+	{
+		std::cout << "find_dir_file() error!\n dir_name = " << dir_name << std::endl;
+		return -1;
+	}
+	while ((dp = readdir(dirp)) != NULL)
+	{
+		if(std::string(dp->d_name) == std::string(".") || std::string(dp->d_name) == std::string(".."))
+		{//当前路径或者父路径
+			continue;
+		}
+		else if(dp->d_type == DT_REG)//文件
+		{
+			file.push_back(std::string(dp->d_name));
+		}
+		else if(dp->d_type == DT_DIR)//路径
+		{
+			dir.push_back(std::string(dp->d_name));
+		}
+	}
+	(void) closedir(dirp);
+	return 0;
+}
+
+int main(int argc, char** argv)
+{
+	std::vector<std::string> dir, file;
+	find_dir_file("./", dir, file);
+	for (std::string strOut : dir)
+		std::cout << "dir:\t" + strOut << std::endl;
+	for (std::string strOut : file)
+		std::cout << "file:\t" + strOut << std::endl;
+}
---
+++ printDirWindows.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <string>
+#include <vector>
+
+//注：VisualStudio需要在“配置属性-高级-高级属性-字符集”下设置“使用多字节字符集”而非“使用 Unicode 字符集”。
+//注：两种字符集的编码方式和使用的FindFirstFile()函数不同。
+//注：使用unicode字符集时需要将(LPCSTR)替换为(LPCWSTR)并将dir_name也转为wchar编码，否则会编译报错或找不到文件。
+//输入文件夹路径dir_name，输出文件夹列表dir与文件列表file
+int find_dir_file(std::string dir_name, std::vector<std::string>& dir, std::vector<std::string>& file)
+{
+	dir.clear();
+	file.clear();
+	WIN32_FIND_DATA FindFileData;
+	dir_name += "/*.*";
+	HANDLE hFind = ::FindFirstFile((LPCSTR)dir_name.c_str(), &FindFileData);
+	if (hFind == INVALID_HANDLE_VALUE)
+	{
+		std::cout << "find_dir_file() error! GetLastError() = " << ::GetLastError() << std::endl;
+		std::cout << "dir :" << dir_name.c_str() << std::endl;
+		return -1;
+	}
+	while (1)
+	{
+		if (FindFileData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)//directory
+		{
+			dir.push_back(std::string((char*)FindFileData.cFileName));
+		}
+		else if (FindFileData.dwFileAttributes & FILE_ATTRIBUTE_ARCHIVE)//file
+		{
+			file.push_back(std::string((char*)FindFileData.cFileName));
+		}
+		if (!FindNextFile(hFind, &FindFileData))
+			break;
+	}
+	::FindClose(hFind);
+	return 0;
+}
+
+int main(int argc, char** argv)
+{
+	std::vector<std::string> dir, file;
+	find_dir_file("./", dir, file);
+	for (std::string strOut : dir)
+		std::cout << "dir:\t" + strOut << std::endl;
+	for (std::string strOut : file)
+		std::cout << "file:\t" + strOut << std::endl;
+}
